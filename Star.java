9af655473c1f7f340e7740202be3f8a5642761d0
Umakant Dodtalle <umakantdodtalle0@gmail.com>
2024-11-22 09:38:03 +0530

    first commit

---
+++ Star.java
@@ -0,0 +1,14 @@
+package com.tka.sample;
+
+public class Star {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("");
+
+			for (int j = 1; j <= 4; j++) {
+				System.out.print("*");
+			}
+		}
+
+	}
+}
