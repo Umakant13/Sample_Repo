a7526a40b91ecc36a8e3e075371ef005dd73a251
Umakant Dodtalle <umakantdodtalle0@gmail.com>
2024-11-22 10:10:32 +0530

    second commit

---
+++ EvenNumbers.java
@@ -0,0 +1,12 @@
+package com.tka.sample;
+
+public class EvenNumbers {
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=150; i++) {
+			if(i%2==0) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
